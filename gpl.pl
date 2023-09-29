bb74f88cf80c88a268ded17c7e60eb00d688b520
MasterSparkInYourFace <stoner9lab@tutanota.com>
2023-09-29 10:36:54 +0300

    GPL headers

--- gpl.pl
+++ gpl.pl
@@ -1,10 +1,24 @@
 #!/usr/bin/env perl
 
-use v5.36;
+# gpl.pl - GPL-3 disclaimer addition tool
+# Copyright (C) 2023  Marisa <stoner9lab@tutanota.com>
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+use v5.34;
 
 use Getopt::Std;
-
-die("usage: $0 <description> <file ...>\n") if (@ARGV < 2);
 
 my %COMMENT_STYLES = (
 	c => [ "/* ", " * ", " */\n" ],
@@ -13,11 +27,13 @@ my %COMMENT_STYLES = (
 );
 
 my %OPTIONS = (
-	c => "c"
+	c => "c",
 );
 $Getopt::Std::STANDARD_HELP_VERSION = 1;
 getopts("c:", \%OPTIONS);
 die("invalid comment style") if (!exists($COMMENT_STYLES{$OPTIONS{c}}));
+
+die("usage: $0 <description> <file ...>\n") if (@ARGV < 2);
 
 my $DESC = shift @ARGV;
 my $EMAIL = 'stoner9lab@tutanota.com';
--- mock.pl
+++ mock.pl
@@ -1,5 +1,21 @@
 #!/usr/bin/env perl
 
+# mock.pl - mock case
+# Copyright (C) 2023  Marisa <stoner9lab@tutanota.com>
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 # hello perlness my old friend
 # writing perl is awful without perldoc or man. why doesn't msysgit come with these
 
